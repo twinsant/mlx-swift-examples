49620782124cc83828e9583be8d0bfaaa68435f0
twinsant <twinsant@gmail.com>
2026-05-08 08:25:27 +0800

    Fix: build for iOS 17.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "mlx-libraries",
-    platforms: [.macOS(.v14), .iOS(.v16)],
+    platforms: [.macOS(.v14), .iOS(.v17)],
     products: [
         .library(
             name: "MLXMNIST",
